115112a85eba5ba15d76a5a1490889d0358ee19a
Ty <justfacey@gmail.com>
2023-01-17 15:16:43 -0800

    connects two cells together by their mid point

--- cell.py
+++ cell.py
@@ -1,7 +1,4 @@
-
-
 from graphics import Line, Point
-
 
 class Cell:
     def __init__(self, window):
@@ -37,3 +34,23 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._window.draw_line(line)
+
+
+    def draw_move(self, to_cell, undo=False):
+        # Get first cell's mid point
+        first_cell_x = (self._x1 + self._x2) / 2
+        first_cell_y = (self._y1 + self._y2) / 2
+        first_cell_mid_point = Point(first_cell_x, first_cell_y)
+
+        # Get second cell's mid point
+        second_cell_x = (to_cell._x1 + to_cell._x2) / 2
+        second_cell_y = (to_cell._y1 + to_cell._y2) / 2
+        second_cell_mid_point = Point(second_cell_x, second_cell_y)
+
+        if undo is False:
+            # use draw_line to draw a red line between two cell
+            line = Line(first_cell_mid_point, second_cell_mid_point)
+            self._window.draw_line(line, fill_color='red')
+        if undo is True:
+            line = Line(first_cell_mid_point, second_cell_mid_point)
+            self._window.draw_line(line, fill_color='grey')            
